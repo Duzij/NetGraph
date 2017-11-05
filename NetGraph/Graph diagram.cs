19d22d2d8c7b794c9e70bcfd44ea0e47456556c2
Maxim Dužij <maxim.duzij@hotmail.com>
2017-11-05 16:23:35 +0100

    packages added, diagram form added

---
+++ NetGraph/Graph diagram.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.GraphViewerGdi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetGraph
+{
+    public partial class Graph_diagram : Form
+    {
+        public Graph graph;
+        public GViewer viewer = new GViewer();
+        public Graph_diagram(Graph graph)
+        {
+            InitializeComponent();
+
+            this.graph = graph;
+            InitializeComponent();
+            viewer.EdgeInsertButtonVisible = false;
+            viewer.MouseDoubleClick += new MouseEventHandler((sender, evt) => {
+                if (viewer.SelectedObject != null)
+                {
+                    var deatiledGraph = new Graph(viewer.SelectedObject.ToString());
+                    Node node = (Node)viewer.SelectedObject;
+                    Node a = new Node("a") { LabelText = node.LabelText };
+                    graph.AddNode(a);
+                    Graph_diagram f = new Graph_diagram(deatiledGraph);
+                    f.ShowDialog();
+                }
+            });
+
+            foreach (var item in graph.Nodes)
+            {
+                item.Attr.LabelMargin = 5;
+            }
+
+            viewer.Graph = graph;
+            SuspendLayout();
+            viewer.Dock = DockStyle.Bottom;
+            Controls.Add(viewer);
+            ResumeLayout();
+        }
+
+        private MouseEventHandler ShowDetail()
+        {
+           
+            return 
+        }
+    }
+}
